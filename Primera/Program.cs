9faa84007f6af69603966b3e04d459db57b051a7
DmytroPoludenko <poludas123@gmail.com>
2021-02-06 11:21:19 +0200

    task3 done

--- Primera/Program.cs
+++ Primera/Program.cs
@@ -7,6 +7,21 @@ namespace Primera
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine(MultArithmeticElements(5, 3, 4));
+        }
+
+        static long MultArithmeticElements (long a, long t, long n)
+        {
+            long b = 0;
+            long sum = 1;
+
+            for (long i = 0; i < n; i++)
+            {
+                b = a + t * i;
+                sum *= b;
+            }
+
+            return sum;
         }
     }
 }
