8e640379fe1db18151e41bcd2db7aea1e058e9ec
DmytroPoludenko <poludas123@gmail.com>
2021-02-06 11:26:46 +0200

    Task 3 done (new class)

--- Primera/Program.cs
+++ Primera/Program.cs
@@ -7,21 +7,8 @@ namespace Primera
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine(MultArithmeticElements(5, 3, 4));
-        }
 
-        static long MultArithmeticElements (long a, long t, long n)
-        {
-            long b = 0;
-            long sum = 1;
-
-            for (long i = 0; i < n; i++)
-            {
-                b = a + t * i;
-                sum *= b;
-            }
-
-            return sum;
-        }
+            Console.WriteLine(Task3.MultArithmeticElements(5, 3, 4));
+        }  
     }
 }
---
+++ Primera/Task3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Primera
+{
+    class Task3: Program
+    {
+        public static long MultArithmeticElements(long a, long t, long n)
+        {
+            long b = 0;
+            long sum = 1;
+
+            for (long i = 0; i < n; i++)
+            {
+                b = a + t * i;
+                sum *= b;
+            }
+
+            return sum;
+        }
+    }
+}
